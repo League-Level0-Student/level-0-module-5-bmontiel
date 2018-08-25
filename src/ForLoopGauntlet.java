41eb8fb302fb735e78bb2d66a0706f57e888c290
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-25 10:53:39 -0700

    ForLoopGauntlet

---
+++ src/ForLoopGauntlet.java
@@ -0,0 +1,31 @@
+
+public class ForLoopGauntlet {
+public static void main(String[] args) {
+//	for (int i = 0; i < 101; i++) {
+//		System.out.println(i);
+//	}
+//	for (int i = 100; i >= 0; i--) {
+//		System.out.println(i);
+//	}
+//	for (int i = 2; i < 101; i+=2) {
+//		System.out.println(i);
+//	}
+//	for (int i = 1; i < 99; i+=2) {
+//		System.out.println(i);
+//	}
+//	for (int i = 1; i < 501; i++) {
+//		if(i%2==0) {
+//			System.out.println(i+" is even");
+//		}
+//		else {
+//			System.out.println(i+" is odd");
+//		}}
+		for (int j = 0; j < 784; j+=7) {
+	System.out.println(j);
+			}
+		for (int i = 2002; i < 2019; i++) {
+			System.out.println();
+		}
+	}
+}
+
