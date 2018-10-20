d5852e72ed77433f6745485cb95f25c513dc0130
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-20 10:53:17 -0700

    PracticeForTest

---
+++ src/PracticeForTest.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class PracticeForTest {
+public static void main(String[] args) {
+String answer=JOptionPane.showInputDialog("Give me a string");	
+}
+static void capitalize(String answer) {
+	System.out.println(answer.toUpperCase());
+}
+static void firstLetter(String answer) {
+	System.out.println(answer.);
+}}
