8c87adfa83041a260726c2615fbefdff5d1d7674
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-03 10:56:37 -0700

    SkillPracticeWorksheet

---
+++ src/SkillPracticeWorksheet.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+import javax.xml.bind.ParseConversionEvent;
+
+public class SkillPracticeWorksheet {
+public static void main(String[] args) {
+//	SkillPractice skills = new SkillPractice();
+//	skills.skill1();
+//	skills.skill2();
+//	skills.skill3();
+//	skills.skill4();
+//	skills.skill5();
+
+//String dimes=JOptionPane.showInputDialog("How many dimes do you have?");
+//int number =Integer.parseInt(dimes);
+//	JOptionPane.showMessageDialog(null,"You have "+ (number*10)+" cents");
+//
+//String tall =JOptionPane.showInputDialog("How tall are you in inches?");
+//int real=Integer.parseInt(tall);
+//if (real<36) {
+//	JOptionPane.showMessageDialog(null, "eat your Wheaties");
+//}
+//else {
+//	JOptionPane.showMessageDialog(null, "Stop eating");
+//}
+
+//for (int i = 0; i < 30; i++) {
+//	if(i%3==0) {
+//		System.out.println(i);
+//	}
+//	
+//}
+ String city =JOptionPane.showInputDialog("What city do you live in?");
+ if (city.equals("San Diego")) {
+	 JOptionPane.showMessageDialog(null, "You live in America's Finest City");
+ }
+	
+	
+	
+	
+	
+}
+}
